c9e0ac87a13505d51c172763050d7de8d891985e
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-30 13:42:56 +0530

    fName validation

--- src/com/usreregistration/UserRegistration.java
+++ src/com/usreregistration/UserRegistration.java
@@ -1,23 +1,25 @@
 package com.usreregistration;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
 	public static void main(String[] args) {
 
-		// TODO Auto-generated method stub
-		Pattern pattern = Pattern.compile(" abc", Pattern.CASE_INSENSITIVE);
-		Matcher matcher = pattern.matcher("hi abce");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first Name: ");
+		String fName = sc.next();
+		Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}$");
+		Matcher matcher = pattern.matcher(fName);
 		boolean matchFound = matcher.find();
 		if(matchFound)
 		{
-			System.out.println("Match Found"+"matcher.start(),matcher.end()");
+			System.out.println("Match Found");
 		}
 		else {
 			System.out.println("Match not Found");
 		}
-
 
 
 	}
